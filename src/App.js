cfb16c3b8fabeb3d62832d9ac5d4ed2ffe1ae508
faizasidd <90434800+faizasidd@users.noreply.github.com>
2022-07-16 16:17:57 -0400

    Revert "started component and fixed the fonts"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from "./logo.svg";
-import "./App.scss";
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Placeholder from "./components/Placeholder/Placeholder";
+import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Placeholder from './components/Placeholder/Placeholder';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
-      <Switch>
-        <Route path="/" exact component={Placeholder} />
-        {/* <Route path="/warehouses" component={Placeholder} /> */}
-        <Route path="/warehouse/:warehouseId" component={Placeholder} />
-        <Route path="/warehouse/edit/:warehouseId" component={Placeholder} />
-        <Route path="/warehouse/add" component={Placeholder} />
-        <Route path="/inventory" component={Placeholder} />
-        <Route path="/inventory/:inventoryId" component={Placeholder} />
-        <Route path="/inventory/edit/:inventoryId" component={Placeholder} />
-        <Route path="/inventory/add" component={Placeholder} />
-      </Switch>
+    {/* <Header /> */}
+    <Switch>
+      <Route path="/" exact component={Placeholder} />
+      {/* <Route path="/warehouses" component={Placeholder} /> */}
+      <Route path="/warehouse/:warehouseId" component={Placeholder} />
+      <Route path="/warehouse/edit/:warehouseId" component={Placeholder} />
+      <Route path="/warehouse/add" component={Placeholder} />
+      <Route path="/inventory" component={Placeholder} />
+      <Route path="/inventory/:inventoryId" component={Placeholder} />
+      <Route path="/inventory/edit/:inventoryId" component={Placeholder} />
+      <Route path="/inventory/add" component={Placeholder} />
+    </Switch>
     </BrowserRouter>
   );
 }
