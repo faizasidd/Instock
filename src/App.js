d6c0fbeae77a982c7bf5717ec60ef26c651936ec
unknown <yikfungseant@gmail.com>
2022-07-16 13:18:51 -0400

    React Router Set Up completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route path="/" exact component={Placeholder} />
+      <Route path="/warehouse-details" component={Placeholder} />
+      <Route path="/:edit-warehouse-details" component={Placeholder} />
+      <Route path="/:add-warehouse" component={Placeholder} />
+      <Route path="/:warehouse-inventory" component={Placeholder} />
+      <Route path="/:warehouse-inventory-details" component={Placeholder} />
+      <Route path="/:warehouse-inventory-edit" component={Placeholder} />
+      <Route path="/:warehouse-inventory-add" component={Placeholder} />
+    </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import '.Header.scss';
+
+function Header() {
+    return (
+        <>
+        <div className='header_wrapper'>
+        
+        </div>
+        </>
+    );
+}
