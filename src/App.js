bd7603b7ea63f2e8950d26f3808932315232ae0d
John Williams <jw@mlmpipa.org>
2022-07-21 20:39:36 -0400

    WarehouseList.js update with dynamic info

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Placeholder from "./components/Placeholder/Placeholder";
 import Header from "./components/Header/Header";
+import WarehouseList from "./components/WarehouseList/WarehouseList";
+import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
+import InventoryList from "./components/InventoryList/InventoryList";
+import InventoryItemDetails from "./components/InventoryItemDetails/InventoryItemDetails";
 import EditWarehouse from "./components/EditWarehouse/EditWarehouse";
 import AddWarehouse from "./components/AddWarehouse/AddWarehouse";
 
@@ -13,7 +17,7 @@ function App() {
       <Header />
       {/* <Header /> */}
       <Switch>
-        <Route path="/" exact component={Placeholder} />
+        <Route path="/" exact component={WarehouseList} />
         {/* <Route path="/warehouses" component={Placeholder} /> */}
         <Route path="/warehouse/:warehouseId" component={Placeholder} />
         <Route path="/warehouse/edit/:warehouseId" component={Placeholder} />
